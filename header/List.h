e6452701e7f9a6c8b05c4a94d512d49bfeacd6b4
DoItMark <18223130@std.stei.itb.ac.id>
2024-11-23 09:28:04 +0700

    nambah register.h, benerin user.h

--- header/List.h
+++ header/List.h
@@ -1,1 +1,16 @@
+#ifndef list_h
+#define list_h
 
+#define Capacity 100
+#define idx_undef -1
+
+typedef int ElType;
+typedef struct{
+    ElType contents[Capacity];
+    int nEff;
+} List;
+
+// definisi List kosong, nEff = 0
+//
+
+#endif
--- header/user.h
+++ header/user.h
@@ -1,5 +1,11 @@
+#ifndef user_h
+#define user_h
+
+#define MAX_LEN 100
+
 typedef struct {
   char name[MAX_LEN];
   char password[MAX_LEN];
   int money;
 } User;
+#endif
