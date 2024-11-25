7853feec74808ac45a66caee00ffd2256fa68d45
Leticia Aldina Trulykinanti <19623214@std.stei.itb.ac.id>
2024-11-25 14:18:32 +0700

    Add files via upload

---
+++ main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+//header ADT
+#include "boolean.h"
+#include "console.h"
+#include "user.h"
+#include "queue.h"
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+#include "list.h"
+#include "arraydin.h"
+
+//header fungsi
+#include "register.h"
+#include "store.h"
+#include "barang.h"
+#include "store_request.h"
+
+//header help
+#include "start.h"
+#include "quit.h"
+#include "save.h"
+#include "load.h"
+#endif 
