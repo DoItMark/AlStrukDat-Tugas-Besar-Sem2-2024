5d7bd5225367e1c85e7fb3645964b23302185edf
DoItMark <18223130@std.stei.itb.ac.id>
2024-11-25 14:20:31 +0700

    nambah banyak macam(store supply,register. dan help)

---
+++ Spesifikasi Program/storesupply/storesupply.c
@@ -0,0 +1,12 @@
+#include "C:\Kuliah ITB 2024-2025\Algoritma Struktu Data\Tugas_Besar_Sem2_2024\AlStrukDat-Tugas-Besar-Sem2-2024\src\ADT\List Dinamis\ListofBarang.h"
+#include "QueueBarang.h"
+#include "mesinkarakter.h"
+#include "mesinkata.h"
+
+void StoreSupply(Queue AntrianBarang, List ListBarang, char *CInput){
+    printf("Apakah Kamu ingin menambah barang %c\n :",&Tail(AntrianBarang));
+    //input user???
+    if (/*input = Terima*/){
+
+    }
+}
