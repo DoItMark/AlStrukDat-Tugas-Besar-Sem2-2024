5648a818dd4755572647f0b3ec31194e35c7933d
Mark(Rakha) <166894547+DoItMark@users.noreply.github.com>
2024-12-16 04:51:32 +0000

    message : "Nambah ADT Map, Stack, listlinier dan mindahin NewUser dari ADT ke src/ADT"

--- src/ADT/User/NewUser.h
+++ src/ADT/User/NewUser.h
@@ -1,8 +1,16 @@
+#ifndef newuser_h
+#define newuser_h
+
+#define MAX_LEN 20
+
 typedef struct {
   char name[MAX_LEN];
   char password[MAX_LEN];
-  integer money;
+  int money;
   Map keranjang;
   Stack riwayat_pembelian;
   Linkedlist wishlist;
 } User;
+
+
+#endif
